4065b0d27676a7d5ec64712b72eece5cc25f2c4d
Paddy Cremen <cremenp@tcd.ie>
2018-07-02 14:00:30 +0000

    add frontend functionality to add user to database

--- accounts/views.py
+++ accounts/views.py
@@ -4,5 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def create_user(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    else:
+        form = UserCreationForm()
+    
     return render(request, 'signup.html', {"form": form})
