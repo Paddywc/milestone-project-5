d99cf1d42b1976c3bfd54c3313c96815beb48f62
Paddy Cremen <cremenp@tcd.ie>
2018-07-03 13:23:36 +0000

    add preliminary header navbar

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
     # from https://www.fomfus.com/articles/how-to-use-email-as-username-for-django-authentication-removing-the-username
     # makes emailed required and have to be unique
-    USERNAME_FIELD = "email"
+    # USERNAME_FIELD = "email"
     email = models.EmailField(('email address'), blank=False, unique=True)
     REQUIRED_FIELDS = []
     
