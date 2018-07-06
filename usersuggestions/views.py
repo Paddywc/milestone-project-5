44add60336c925126cc9a200c760db54954bd739
Paddy Cremen <cremenp@tcd.ie>
2018-07-06 17:35:33 +0000

    add JavaScript function to change button on AddSuggestion form based on form is_feature value

--- usersuggestions/views.py
+++ usersuggestions/views.py
@@ -11,7 +11,12 @@ def add_suggestion(request):
     form = SuggestionForm(initial={"user": request.user})
     if request.method=="POST":
         form = SuggestionForm(data=request.POST)
+        # below line of code returns true if suggestion_type ==
+        # 'feature'. Returns False if == 'bug fix'. Returned as String
+        is_feature = request.POST.get("suggestion_type")
         if form.is_valid():
+            if is_feature=='True':
+                print("this is running")
             form.save()
     
     return render(request, 'add_suggestion.html', {"form": form})
