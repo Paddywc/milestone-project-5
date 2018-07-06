ed1009fd06523a49e16c0cd7e6e83cf684bcae67
Paddy Cremen <cremenp@tcd.ie>
2018-07-06 11:47:30 +0000

    remove virtual environment folder

--- usersuggestions/models.py
+++ usersuggestions/models.py
@@ -6,7 +6,7 @@ class Suggestion(models.Model):
     feature = models.BooleanField(blank=False, default=False)
     user = models.ForeignKey(User, null=False, on_delete=models.PROTECT)
     title = models.CharField(max_length=200, blank=False)
-    details = models.TextField()
+    details = models.CharField(max_length=200, blank=False)
     date_time = models.DateTimeField(auto_now_add=True)
     
     
