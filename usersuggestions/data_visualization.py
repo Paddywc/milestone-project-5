68d7ea40e5f21af4e61aa2af0ac1b7b12e2a887a
Paddy Cremen <cremenp@tcd.ie>
2018-07-18 16:55:40 +0000

    get Django and python imports working in jupyter

--- usersuggestions/data_visualization.py
+++ usersuggestions/data_visualization.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+from .models import Suggestion
+from django.db.models import Count
 
-plt.plot([1,2,3],[5,7,4])
-
-plt.show()
+def get_highest_vote_totals(limit):
+    """
+    """
+    return Suggestion.objects.filter(is_feature=True, suggestionadminpage__in_current_voting_cycle=True).annotate(upvotes=Count("upvote")).annotate(comments=Count("comment")).order_by("-upvotes")[:limit]
+    
