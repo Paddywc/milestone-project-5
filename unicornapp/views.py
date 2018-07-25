57aee8ede09918447a24fc35a40765bad7839537
Paddy Cremen <cremenp@tcd.ie>
2018-07-25 12:46:41 +0000

    add login form to homepage

--- unicornapp/views.py
+++ unicornapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from accounts.forms import UserSignupForm
+from django.contrib.auth import login
+from market.coins import add_coins
+from django.conf import settings
+import market.coin_prices.coin_rewards as coin_rewards
 
 # Create your views here.
 def render_home(request):
     """
     """
-    return render(request, "home.html")
+    form = UserSignupForm
+    if request.method == "POST":
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            if settings.COINS_ENABLED:
+                add_coins(new_user, coin_rewards.signup, 5)
+                login(request, new_user)
+    return render(request, "home.html", {"form": form})
